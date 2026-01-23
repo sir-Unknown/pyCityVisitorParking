0646a8f59b41ce1ee7f92e3ede0708b1f6f33528
sir-Unknown <sir-Unknown@users.noreply.github.com>
2026-01-23 09:25:59 +0000

    Use v1 permit endpoints for Amsterdam discovery

--- src/pycityvisitorparking/provider/amsterdam/const.py
+++ src/pycityvisitorparking/provider/amsterdam/const.py
@@ -4,9 +4,9 @@ DEFAULT_API_URI = "/api"
 
 LOGIN_ENDPOINT = "/ssp/login_check"
 CLIENT_PRODUCT_ENDPOINT = "/v1/client_product/{client_product_id}"
-PERMIT_LIST_ENDPOINT = "/permit/list"
-PERMIT_LIST_FOR_CLIENT_ENDPOINT = "/permit/list_for_client"
-PERMIT_OVERVIEW_PRODUCT_LIST_ENDPOINT = "/permit_overview/product_list"
+PERMIT_LIST_ENDPOINT = "/v1/permit/list"
+PERMIT_LIST_FOR_CLIENT_ENDPOINT = "/v1/permit/list_for_client"
+PERMIT_OVERVIEW_PRODUCT_LIST_ENDPOINT = "/v1/permit_overview/product_list"
 
 PARKING_SESSION_LIST_ENDPOINT = "/v1/ssp/parking_session/list"
 PARKING_SESSION_START_ENDPOINT = "/v1/ssp/parking_session/start"
